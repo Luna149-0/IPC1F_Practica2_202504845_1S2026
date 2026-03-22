1f22a9fb13ce1bb04f9536eafe506d891a075186
Ixchel Panjoj <ixchelpanjoj@gmail.com>
2026-03-22 02:35:47 -0600

    Segundo Commit de prueba

--- src/practica2_harry_potter/Practica2_Harry_Potter.java
+++ src/practica2_harry_potter/Practica2_Harry_Potter.java
@@ -5,7 +5,8 @@ import practica2_Views.vista_MenuPrincipal;
 
 public class Practica2_Harry_Potter {
 
-
+//Mostrando menu principal
+    
     public static void main(String[] args) {
           vista_MenuPrincipal vista= new vista_MenuPrincipal();
         vista.setVisible(true);
