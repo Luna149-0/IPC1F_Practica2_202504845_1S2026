648bdb1e4d004f43a59eb630835e59b3f25f0b0d
Ixchel Panjoj <ixchelpanjoj@gmail.com>
2026-03-22 02:32:41 -0600

    Primer Commit, ya hice el menu

---
+++ src/practica2_harry_potter/Practica2_Harry_Potter.java
@@ -0,0 +1,16 @@
+
+package practica2_harry_potter;
+
+import practica2_Views.vista_MenuPrincipal;
+
+public class Practica2_Harry_Potter {
+
+
+    public static void main(String[] args) {
+          vista_MenuPrincipal vista= new vista_MenuPrincipal();
+        vista.setVisible(true);
+        vista.setLocationRelativeTo(null);
+
+    }
+    
+}
